4c50638a92cab4cd64dd6006e733a5ac4e81996a
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-03-16 17:37:17 -0700

    Betty zise

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", ziseof(char));
-	printf("size of a int: %zu byte(s)\n", ziseof(int));
-	printf("zise of a long int: %zu byte(s)\n", ziseof(long int));
-	printf("zise of a long long int: %zu byte(s)\n", ziseof(long long int));
-	printf("zise of a float: %zu byte(s)\n", ziseof(float));
+	printf("Size of a char: %lu byte(s)\n", ziseof(char));
+	printf("size of a int: %lu byte(s)\n", ziseof(int));
+	printf("zise of a long int: %lu byte(s)\n", ziseof(long int));
+	printf("zise of a long long int: %lu byte(s)\n", ziseof(long long int));
+	printf("zise of a float: %lu byte(s)\n", ziseof(float));
 	return (0);
 }
