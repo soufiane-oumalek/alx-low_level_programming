bec918179f604e52d7409686ab28ea5cac8432ba
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-03-21 10:55:46 -0700

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	_puchar('_')
-	_puchar('p')
-	_puchar('u')
-	_puchar('t')
-	_puchar('c')
-	_puchar('h')
-	_puchar('a')
-	_puchar('r')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
