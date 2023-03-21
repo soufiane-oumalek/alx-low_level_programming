f0cdee051b3f7f2c5ea89d3f20e3a322b9e41152
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-03-21 10:45:57 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *description - prints _putchar
+ *Return: Always 0
+ */
+int main(void)
+{
+	_puchar('_')
+	_puchar('p')
+	_puchar('u')
+	_puchar('t')
+	_puchar('c')
+	_puchar('h')
+	_puchar('a')
+	_puchar('r')
+}
+
