d0c97c5c4de37d5760a81933c93d6331e8ad2d78
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-03-23 07:41:30 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper character
+ * @c : check to character case
+ * Return: 1 for upper 0 for any else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
