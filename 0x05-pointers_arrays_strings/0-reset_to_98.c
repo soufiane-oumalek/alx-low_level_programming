238800a90febe246eea98c6b4b0a6c834aabafd2
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-03-27 10:44:56 -0700

    Frist int pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ * @: input an int pointer
+ */
+
+
+void reset_to_98(int *n);
+{
+	*n = 98
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers and array and strings
