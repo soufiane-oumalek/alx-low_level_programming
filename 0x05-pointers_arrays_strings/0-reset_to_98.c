019cafcff03060fbef7b433f30fcb11252d1738a
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-03-27 10:52:43 -0700

    Frist int pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
-/*
- * reset_to_98 - takes a pointer to an int as parameter and updates
- * @: input an int pointer
+
+/**
+ * reset_to_98 - update the value of n by pointing to 98
+ * @n: pointer to an int to update
  */
 
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
 
