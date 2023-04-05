e0788cb0f6a2f7449154aa291b0fb46872e94fd9
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-05 07:31:14 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string
+ * @s: print string
+ * Return 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
+
