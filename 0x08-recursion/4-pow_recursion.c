8b828b16c7cec4b4794f39c433e3073b579c1c31
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-05 13:18:51 -0700

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: number rised
  * @y: power
- * Returne: power y
+ * Return: x _pow_recursion
  */
 int _pow_recursion(int x, int y)
 {
