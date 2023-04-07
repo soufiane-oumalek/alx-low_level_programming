1c9c22975a86c9e5fe52228453f2bf38e45ad761
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-06 19:07:51 -0700

    sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Entry function
- * @n: nuumber
- * Return: x
+ * _sqrt_recursion_helper - Entry Functions
+ * @n: num
+ * @v: num
+ * Return: int
  */
-int _sqrt_recursion_helper(int n, int vl)
+int _sqrt_recursion_helper(int n, int v)
 {
 	if (n == 0 || n == 1)
 		return (1);
-	if (n == vl || n < 0)
+	if (n == v || n < 0)
 		return (-1);
-	if (vl * vl == n)
-		return (vl);
+	if (v * v == n)
+		return (v);
 
-	return (_sqrt_recursion_helper(n, vl + 1));
+	return (_sqrt_recursion_helper(n, v + 1));
 
 }
 
