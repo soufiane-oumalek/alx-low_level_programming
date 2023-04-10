e304f9993bc3fc42a723aff526fef641347908bf
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-10 08:23:48 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,18 @@ int main(int argc, char **argv)
 {
 	int j, k;
 
-	if (argc < 3)
+	if (argc == 1 || argc == 2)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
-	j = atoi(argv[1]);
-	k = atoi(argv[2]);
-	printf("%d\n", j * k);
-	return (0);
+	else
+	{
+		j = 1;
+
+		for (j = 1; j < 3; i++)
+			k *= atoi(argv[i]);
+
+		printf("%d\n", k);
 }
 
