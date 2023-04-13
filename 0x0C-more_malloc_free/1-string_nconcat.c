719922afb6d603522921832d8b41caa4fb4d9b82
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-13 09:11:50 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number
+ * *string_nconcat - concatenates 2 strings
+ * @s1: pointer for the first string
+ * @s2: pointer for the second string
+ * @n: number bytes
  * Return: pointer shall point to a newly allocated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i, j, k;
+	unsigned int index1, index2, length1, length2;
+	char *s;
 
 	if (s1 == 0)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[1]; i++)
-			;
-	}
+		s1 = "";
 	if (s2 == 0)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
+		s2 = "";
+	length1 = 0;
 
-	if (j > n)
-		j = n;
+	while (*(s1 + length1))
+		length1++;
+	length2 = 0;
 
-	str = malloc(sizeof(char) * (i + j + 1));
-	if (str == 0)
+	while (*(s2 + length2))
+		length2++;
+	if (n >= length2)
+		n = length2;
+	s = malloc(sizeof(char) * (length1 + n + 1));
+	if (s == 0)
 		return (0);
-
-	for (k = 0; k < i; k++)
-		str[k] = s1[k];
-
-	for (k = 0; k < j; k++)
-		str[k + i] = s2[k];
-
-	str[i + j] = '\0';
-	return (str);
+	for (index1 = 0; index1 < length1; index1++)
+		*(s + index1) = *(s1 + index1);
+	for (index2 = 0; index2 < n; index2++, index1++)
+		*(s + index1) = *(s2 + index2);
+	*(s + index1) = '\0';
+	return (s);	
 }
 
