b058c699f3ec7ade80ab90ab4839dffc87a71d9d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-17 08:14:00 -0700

    dog_t

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - describe a dog 
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ * description: to a dog
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
