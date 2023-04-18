4cdf7c4790d70f8276a7c16d7c765f4d7bb28801
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-17 20:40:52 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
+	/*allocate memory for struct new_dog*/
 	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
 
+	/*allocate memory for name of new_dog*/
 	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 
 	if (new_dog->name == NULL)
@@ -27,8 +29,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	/*put age*/
 	new_dog->age = age;
 
+	/*alocate memory for owner's new_dog*/
 	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 
 	if (new_dog->owner == NULL)
@@ -37,6 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
+	/*cpy name and owner string*/
 	new_dog->name = strcpy(new_dog->name, name);
 	new_dog->owner = strcpy(new_dog->owner, name);
 	return (new_dog);
