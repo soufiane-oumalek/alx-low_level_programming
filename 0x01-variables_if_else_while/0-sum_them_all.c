79b6d75ff06b45a2f21d16962413f2574719212e
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-20 08:08:52 -0700

    sum_them_all

---
+++ 0x01-variables_if_else_while/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: argument number
+ * Return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list argv;
+
+	unsigned int i;
+	int S = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(argv, n);
+
+	for (i = 0; i < n; i++)
+	{
+		S += va_arg(argv, int);
+	}
+	va_end(argv);
+
+	return (S);
+}
+
+
+
