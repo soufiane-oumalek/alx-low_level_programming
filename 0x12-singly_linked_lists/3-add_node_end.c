449dc809646dfbffd6f56f424241bb5d57424c3b
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-27 19:06:02 -0700

    add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,11 +12,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *n, *node;
 
-	if (!head || !str)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
 	n = malloc(sizeof(list_t));
-		if (!n)
+		if (n == NULL)
 			return (NULL);
 
 	n->str = strdup(str);
@@ -28,15 +28,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	n->len = strlen(str);
 	n->next = (NULL);
 
-	if (!*head)
+	if (*head == NULL)
 	{
 		*head = n;
-		return (n);
 	}
-	node = *head;
-	while (node->next != NULL)
-		node = node->next;
-	node = n;
+	else
+	{
+		node = *head;
+		while (node->next != NULL)
+			node = node->next;
+		node->next = n;
+	}
 	return (n);
 }
 
