75fb105ac0d04a7efad90073d53fce342a4adc5a
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-05-08 16:58:55 -0700

    append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
-	if (fd < 0)
+	if (fd == -1)
 	{
 		return (-1);
 	}
