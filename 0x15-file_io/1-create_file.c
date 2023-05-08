faa9bd4845289494f7f188e9a204f8865baacd6d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-05-08 09:40:52 -0700

    creat_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		ssize_t b_writt = write(fd, text_content, strlen(text_content));
+
 		if (b_writt == -1 || (size_t)b_writt != strlen(text_content))
 		{
 			r = -1;
