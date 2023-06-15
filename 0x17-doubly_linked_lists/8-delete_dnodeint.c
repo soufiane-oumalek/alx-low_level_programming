c7cddb39d57a0d04770815ac7eb40486a06ca28a
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-15 11:39:47 -0700

    delete node int

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index
+ * of a dlistint_t linked list.
+ * @head: linked list
+ * @index: index delete nod
+ * Return: 1 if success, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *ptr;
+
+	if (head == NULL)
+		return (-1);
+
+	ptr = *head;
+
+	if (ptr == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		ptr = ptr->next;
+		if (ptr == NULL)
+			return (-1);
+	}
+
+	if (ptr->prev != NULL)
+		ptr->prev->next = ptr->next;
+	else
+	{
+		*head = ptr->next;
+		if (ptr->next != NULL)
+			ptr->next->prev = NULL;
+		free(ptr);
+	}
+	return (1);
+}
