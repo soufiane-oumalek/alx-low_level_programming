54414f83efb78347100ee55affac132e8b0a9acb
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-06 22:00:40 +0000

    tsk_3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_hash_node;
-	unsigned long int index, i;
+	unsigned long int index;
 	char *v_c;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
@@ -22,16 +22,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	i = index;
-	while (ht->array[i])
+	
+	if (ht->array[index] && !(strcmp(ht->array[index]->key, key)))
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = v_c;
-			return (1);
-		}
-		i++;
+		free(ht->array[index]->value);
+		ht->array[index]->value = v_c;
+		return (1);
 	}
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (!new_hash_node)
