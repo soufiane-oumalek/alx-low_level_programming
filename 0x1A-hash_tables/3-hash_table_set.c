56b20af1d5307d33b154a3c70cf28d5602bbb60e
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-06 22:02:02 +0000

    tsk_3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	if (ht->array[index] && !(strcmp(ht->array[index]->key, key)))
 	{
 		free(ht->array[index]->value);
