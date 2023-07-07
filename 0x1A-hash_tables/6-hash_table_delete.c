93494471d882fae3bb2101373bcebdc7799992e6
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-07 03:15:22 +0000

    tsks

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,13 +7,13 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int index = 0;
-	hash_node_t *hash_node_ptr, *p;
+	hash_node_t *node_ptr, *p;
 
 	while (index < ht->size)
 	{
 		if (ht->array[index] != NULL)
 		{
-			for (hash_node_ptr = ht->array[index]; hash_node_ptr != NULL; hash_node_ptr = p)
+			for (node_ptr = ht->array[index]; node_ptr != NULL; node_ptr = p)
 			{
 				p = hash_node_ptr->next;
 				free(hash_node_ptr->key);
